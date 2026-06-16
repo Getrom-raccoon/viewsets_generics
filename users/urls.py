34961165a131a36be37cb17d46e9222ef3905663
getrom-raccoon <getromgleb@gmail.com>
2026-06-16 23:54:13 +0300

    add project

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PaymentViewSet, ProfileView
+from .views import UserViewSet, UserRegistrationView, PaymentViewSet, ProfileView
 
 router = DefaultRouter()
 router.register(r'payments', PaymentViewSet, basename='payment')
+router.register(r'', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('register/', UserRegistrationView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('', include(router.urls)),
 ]
